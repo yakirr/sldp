97273048925cf45bf7e4f91a67ca401e51971c4d
Reshef, Yakir <yakir@seas.harvard.edu>
2017-11-01 20:03:42 -0400

    fixed script/pip issue

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sldp',
-    version='1.0.5',
+    version='1.0.6',
     description='Signed LD profile regression',
     url='http://github.com/yakirr/sldp',
     author='Yakir Reshef',
