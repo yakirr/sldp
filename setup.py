26fcd5643d0a2ec057c7c58774de9777a2bb6511
Reshef, Yakir <yakir@seas.harvard.edu>
2017-11-02 01:12:55 -0400

    updated version string

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sldp',
-    version='1.0.6',
+    version='1.0.7',
     description='Signed LD profile regression',
     url='http://github.com/yakirr/sldp',
     author='Yakir Reshef',
