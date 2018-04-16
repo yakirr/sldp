da4e1aad7f15d3516bf44e1dcd8c43a8b474d8ae
Reshef <yar2@login04.o2.rc.hms.harvard.edu>
2018-04-16 10:32:57 -0400

    increment version num

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sldp',
-    version='1.1.1',
+    version='1.1.2',
     description='Signed LD profile regression',
     url='http://github.com/yakirr/sldp',
     author='Yakir Reshef',
